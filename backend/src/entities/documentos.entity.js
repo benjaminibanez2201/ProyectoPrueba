9c71fa7d979f9ab09c3f456f5075c5e4f7e29226
javimiau <javiera.carrasco2201@alumnos.ubiobio.cl>
2025-11-14 21:18:42 -0300

    Implementación entidad documentos para RF2

---
+++ backend/src/entities/documentos.entity.js
@@ -0,0 +1,43 @@
+import { EntitySchema } from "typeorm";
+
+export const DocumentosPractica = new EntitySchema({
+    name: "DocumentoPractica", // Nombre del objeto en TypeORM
+    tableName: "documentos_practicas", // Nombre de la tabla en la BDD
+    columns: {
+        id: {
+            primary: true,
+            type: "int",
+            generated: "increment",
+        },
+        tipo: { // puede ser 'Informe', 'Bitácora', 'Evaluación', 'Formulario', etc.
+            type: "varchar",
+            length: 50,
+            nullable: false,
+        },
+        estado: { // 'pendiente', 'enviado', 'aprobado'
+            type: "varchar",
+            length: 50,
+            default: "pendiente",
+        },
+        ruta_archivo: { // para Bitácoras o Informes (archivos subidos)
+            type: "varchar",
+            nullable: true, 
+        },
+        datos_json: { // para Evaluaciones o Formularios (datos rellenados)
+            type: "jsonb",
+            nullable: true,
+        },
+        fecha_creacion: {
+            type: "timestamp",
+            createDate: true,
+        },
+    },
+    relations: {
+        practica: { 
+            target: "Practica",
+            type: "many-to-one",
+            joinColumn: { name: "practicaId" }, 
+            inverseSide: "documentos", 
+        },
+    }
+});
