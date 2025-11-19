b9cc11a18f89b8a99d5ae364fda734d4a660d7a2
javimiau <javiera.carrasco2201@alumnos.ubiobio.cl>
2025-11-19 16:58:36 -0300

    Agrega rutas de RF1

--- backend/src/routes/empresa.routes.js
+++ backend/src/routes/empresa.routes.js
@@ -1,5 +1,7 @@
 import express from "express";
 import {
+  validarTokenEmpresa,
+  confirmarInicioPractica,
   generarTokenEmpresa,
   verFormulario,
   enviarEvaluacion,
@@ -16,5 +18,9 @@ router.get("/ver-formulario", validarTokenEmpresa, verFormulario);
 
 // Enviar evaluación (empresa envía datos)
 router.post("/enviar-evaluacion", validarTokenEmpresa, enviarEvaluacion);
+
+// Validar token (empresa verifica su token)
+router.get("/validar-token", validarTokenEmpresa);
+router.post("/confirmar-inicio-practica", confirmarInicioPractica)
 
 export default router;
